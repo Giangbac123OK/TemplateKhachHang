3d9e1022b7f7275ffed989ff6ea0dbd04bec1425
HoàngThanh Giang <hoangthanhgiang22@gmail.com>
2024-12-05 15:37:37 +0700

    giang ok

--- Controllers/thongke.js
+++
@@ -1,7 +0,0 @@
-app.controller('thongkeController', function($scope, $http) {
-    $scope.userInfo = JSON.parse(localStorage.getItem('userInfo'));
-    $http.get('https://localhost:7297/api/Hoadon')
-    .then(function(response){
-        
-    })
-});
